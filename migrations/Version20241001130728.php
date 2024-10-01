861f3b47012b75d1ad1745752f0613268325fc73
Elamine <insanakibelamine@outlook.fr>
2024-10-01 16:12:25 +0200

    implementation user ticket

--- migrations/Version20241001130728.php
+++ migrations/Version20241001130728.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240930141955 extends AbstractMigration
+final class Version20241001130728 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20240930141955 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD firstname VARCHAR(255) NOT NULL, ADD lastname VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE ticket CHANGE state state VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user RENAME INDEX uniq_email TO UNIQ_IDENTIFIER_EMAIL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP firstname, DROP lastname');
+        $this->addSql('ALTER TABLE user RENAME INDEX uniq_identifier_email TO UNIQ_EMAIL');
+        $this->addSql('ALTER TABLE ticket CHANGE state state VARCHAR(255) DEFAULT \'En vente\'');
     }
 }
