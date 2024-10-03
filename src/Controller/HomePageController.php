72d8184b8536bcb3414c2c38a97c1f0df0934de4
JohanDelem <you@example.com>
2024-10-03 16:26:53 +0200

    logo ok

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,27 +4,57 @@ namespace App\Controller;
 
 use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 
-
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(TicketRepository $ticketRepository, #[MapQueryParameter] ?string $query): Response
+    public function index(TicketRepository $ticketRepository, HttpClientInterface $httpClient, #[MapQueryParameter] ?string $query): Response
     {
-         $tickets = $ticketRepository->findTicketsEnVente($query);
-        // $tickets = $ticketRepository->findAll();
+        $tickets = $ticketRepository->findTicketsEnVente($query);
+          
+        $allLogos = [];
+
+        foreach ($tickets as $ticket) {
+            $homeTeam = $ticket->getHomeTeam();
+            $awayTeam = $ticket->getAwayTeam();
+
+            // Récupération des logos avec un cache local pour éviter les appels répétitifs
+            // Si l'équipe n'existe pas déjà en tant que clé dans mon tableau alors je fait la requete
+            if (!array_key_exists($homeTeam, $allLogos)) {
+                $allLogos[$homeTeam] = $this->getTeamLogo($homeTeam, $httpClient);
+            }
+
+            if (!array_key_exists($awayTeam,$allLogos)) {
+                $allLogos[$awayTeam] = $this->getTeamLogo($awayTeam, $httpClient);
+            }
+        }
+
         return $this->render('home_page/index.html.twig', [
-            'tickets' => $tickets
+            'tickets' => $tickets,
+            'teamsLogo' => $allLogos,
         ]);
     }
+
+    private function getTeamLogo(string $teamName, HttpClientInterface $httpClient): ?string
+    {
+        try {
+            $response = $httpClient->request('GET',
+                'https://apiv2.allsportsapi.com/football/?met=Teams&teamName=' . $teamName . '&APIkey=' . $this->getParameter('app.api_key')
+            );
+            $data = $response->toArray();
+
+            return $data['result'][0]['team_logo'] ?? null;
+        } catch (\Exception $e) {
+            // Loggez l'erreur si nécessaire
+            $this->get('logger')->error('Erreur lors de la récupération du logo de l\'équipe ' . $teamName . ': ' . $e->getMessage());
+            return null;
+        }
+    }
 }
-
-
-
 
 
 
